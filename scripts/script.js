782303c70682a1c14da7f54d06c900f7c6b4b29a
NDC Studio <ndc.dev.code@gmail.com>
2025-03-12 09:58:06 +0100

    1e Objet

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,18 @@
-/*
-*/
+const guitar_collection = [
+  // 1
+  {
+    name: "JEM Steve Vai Signature",
+    brand: "Ibanez",
+    model: "JEMJRL-WH",
+    pickups: {
+      neck: "Quantum (H)",
+      middle: "Quantum (S)",
+      bridge: "Quantum (H)",
+    },
+    body: "Meranti",
+    neck: "Érable",
+    frets: 24,
+    picture: ""
+  },  
+
+];
