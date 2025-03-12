4e91f4d49af00caf51ca79cdd933d43a43ba72f1
NDC Studio <ndc.dev.code@gmail.com>
2025-03-12 10:07:58 +0100

    3e Objet

--- scripts/script.js
+++ scripts/script.js
@@ -12,8 +12,9 @@ const guitar_collection = [
     body: "Meranti",
     neck: "Érable",
     frets: 24,
-    picture: ""
-  },  
+    vibrato : "Floyd Rose",
+    picture: "../assets/jem.png"
+  },
   // 2
   {
     name: "Les Paul Custom Alpine White Gloss",
@@ -26,7 +27,23 @@ const guitar_collection = [
     body: "Acajou",
     neck: "Érable",
     frets: 22,
-    picture: ""
+    picture: "../assets/lespaul.jpg"
+  },
+  // 3
+  {
+    name: "70th Anniversary Player Stratocaster, Nebula Noir RW",
+    brand: "Fender",
+    model: "0717669564612",
+    pickups: {
+        neck: "Pure Vintage '59 Single-Coil Strat",
+        middle: "Pure Vintage '59 Single-Coil Strat",
+        bridge: "Pure Vintage '59 Single-Coil Strat",
+      },
+    body: "Aulne",
+    neck: "Érable",
+    frets: 22,
+    vibrato : "Tremolo",
+    picture: "../assets/strato.jpg"
   },
 
 ];
