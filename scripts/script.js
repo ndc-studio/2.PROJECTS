c7608aa494d70a1d02df904228aab8cb0047fc6e
NDC Studio <ndc.dev.code@gmail.com>
2025-03-12 14:39:28 +0100

    Modification titre et background image de body

--- scripts/script.js
+++ scripts/script.js
@@ -1,161 +1,50 @@
-const guitar_collection = [
-  // 1
-  {
-    name: "JEM Steve Vai Signature",
-    brand: "Ibanez",
-    model: "JEMJRL-WH",
-    pickups: {
-      neck: "Quantum (H)",
-      middle: "Quantum (S)",
-      bridge: "Quantum (H)",
-    },
-    body: "Meranti",
-    neck: "Érable",
-    frets: 24,
-    vibrato : "Floyd Rose",
-    picture: "../assets/jem.png"
-  },
-  // 2
-  {
-    name: "Les Paul Custom Alpine White Gloss",
-    brand: "Gibson",
-    model: "GIT0048896-000",
-    pickups: {
-      neck: "Custom Bucker, Alnico III",
-      middle: "Sans",
-      bridge: "Custom Bucker, Alnico III",
-    },
-    body: "Acajou",
-    neck: "Érable",
-    frets: 22,
-    vibrato : "Sans",
-    picture: "../assets/lespaul.jpg"
-  },
-  // 3
-  {
-    name: "70th Anniversary Player Stratocaster, Nebula Noir RW",
-    brand: "Fender",
-    model: "0717669564612",
-    pickups: {
-      neck: "Pure Vintage '59 Single-Coil Strat",
-      middle: "Pure Vintage '59 Single-Coil Strat",
-      bridge: "Pure Vintage '59 Single-Coil Strat",
-    },
-    body: "Aulne",
-    neck: "Érable",
-    frets: 22,
-    vibrato : "Tremolo",
-    picture: "../assets/strato.jpg"
-  },
-  // 4
-  {
-    name: "SG Angus Young Signature Model",
-    brand: "Gibson",
-    model: "022770341",
-    pickups: {
-      neck: "humbucker",
-      middle: "Sans",
-      bridge: "humbucker",
-    },
-    body: "Acajou",
-    neck: "Acajou",
-    frets: 22,
-    vibrato : "Tremolo Maestro Vibrola",
-    picture: "../assets/sg.jpg"
-  },
-  // 5
-  {
-    name: "Player II Telecaster RW Transparent Cherry",
-    brand: "Fender",
-    model: "GIT0061904-002",
-    pickups: {
-      neck: "Player Series Alnico 5",
-      middle: "Sans",
-      bridge: "Player Series Alnico 5",
-    },
-    body: "Boulot",
-    neck: "Érable",
-    frets: 22,
-    vibrato : "Tremolo Maestro Vibrola",
-    picture: "../assets/telecaster.jpg"
-  },
-  // 6
-  {
-    name: "Guitars Archtop White Larry Carlton",
-    brand: "Sire",
-    model: "H7/WH",
-    pickups: {
-      neck: "humbuckers Alnico 2",
-      middle: "Sans",
-      bridge: "humbuckers Alnico 2",
-    },
-    body: "Acajou",
-    neck: "Érable",
-    frets: 22,
-    picture: "../assets/archtop.jpg"
-  },
-  // 7
-  {
-    name: "Flying V Ruby Red Metallic",
-    brand: "Epiphone",
-    model: "GIT0061044-000",
-    pickups: {
-      neck: "ProBucker-2",
-      middle: "Sans",
-      bridge: "ProBucker-3",
-    },
-    body: "Acajou",
-    neck: "Acajou",
-    frets: 22,
-    vibrato : "Sans",
-    picture: "../assets/flyingv.jpg"
-  },
-  // 8
-  {
-    name: "Artwood Vintage 12-String",
-    brand: "Ibanez",
-    model: "GIT0044509-000",
-    pickups: {
-      neck: "Sans",
-      middle: "Sans",
-      bridge: "AEQ-SP2",
-    },
-    body: "Acajou",
-    neck: "Acajou",
-    frets: 20,
-    vibrato : "Sans",
-    picture: "../assets/12str.jpg"
-  },
-  // 9
-  {
-    name: "000-28EC Eric Clapton Signature",
-    brand: "Martin Guitars",
-    model: "GIT0059278-000",
-    pickups: {
-      neck: "Sans",
-      middle: "Sans",
-      bridge: "En option.."
-    },
-    body: "Ébène",
-    neck: "Épicéa",
-    frets: 20,
-    vibrato : "Sans",
-    picture: "../assets/acoustic.jpg"
-  },
-  // 10
-  {
-    name: "CN-60S Natural WN",
-    brand: "Fender",
-    model: "455298",
-    pickups: {
-      neck: "Sans",
-      middle: "Sans",
-      bridge: "Sans"
-    },
-    body: "Acajou",
-    neck: "Acajou",
-    frets: 18,
-    vibrato : "Sans",
-    picture: "../assets/classic.jpg"
-  }
-];
+guitarCollection.forEach(function(guitar) {
+    const card = document.createElement("div");
+    card.classList.add('guitar-card');
+
+    const guitarName = document.createElement("h3");
+    guitarName.textContent = `${guitar.name}`;
+    guitarName.classList.add('title-card');
+    card.appendChild(guitarName);
+
+    const guitarBrand = document.createElement("h3");
+    guitarBrand.textContent = `Marque: ${guitar.brand}`;
+    card.appendChild(guitarBrand);
+
+    const guitarModel = document.createElement("h4");
+    guitarModel.textContent = `Modèle: ${guitar.model}`;
+    card.appendChild(guitarModel);
+
+    const guitarPickups = document.createElement('p');
+    guitarPickups.innerHTML = `- Micro manche: ${guitar.pickups.neck}<br>- Micro mid: ${guitar.pickups.middle}<br>- Micro chevalet: ${guitar.pickups.bridge}`;
+    guitarPickups.classList.add('pickups');
+    card.appendChild(guitarPickups);
+
+    const guitarNeck = document.createElement("p");
+    guitarNeck.textContent = `Manche: ${guitar.neck}`;
+    guitarNeck.classList.add('neck');
+    card.appendChild(guitarNeck);
+
+    const guitarFrets = document.createElement("p");
+    guitarFrets.textContent = `Nbr de frets: ${guitar.frets}`;
+    guitarFrets.classList.add('frets');
+    card.appendChild(guitarFrets);
+    
+    const guitarBody = document.createElement("p");
+    guitarBody.textContent = `Corps: ${guitar.body}`;
+    guitarBody.classList.add('guitar-body');
+    card.appendChild(guitarBody);
+
+    const guitarVibrato = document.createElement("p");
+    guitarVibrato.textContent = `Vibrato: ${guitar.vibrato}`;
+    guitarVibrato.classList.add('vibrato');
+    card.appendChild(guitarVibrato);
+
+    const guitarPicture = document.createElement("img");
+    guitarPicture.src = guitar.picture;
+    guitarPicture.alt = `Photo de ${guitar.name}`;
+    card.appendChild(guitarPicture);
+
+    const container = document.getElementById("guitar-container");
+    container.appendChild(card);
+});
